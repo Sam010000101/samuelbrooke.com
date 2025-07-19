1f8b15c849edd78ef6df6d1e02872e04d01a22bb
Sam010000101 <reposme@outlook.com>
2025-07-19 08:03:46 +0100

    Setting up project to deploy to github pages

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,12 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
-  // output: "export",
+  output: "export",
   // Optional: if your repo is a project page
-  // basePath: '/your-repo-name',
-  // assetPrefix: '/your-repo-name',
+  basePath: isProd ? "/samuelbrooke.com" : "",
+  assetPrefix: isProd ? "/samuelbrooke.com" : "",
   // Optional: Remove trailing slashes if needed
   // trailingSlash: true,
 };
