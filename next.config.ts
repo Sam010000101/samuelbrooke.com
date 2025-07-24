ef2abca0bc35537a04aa7f8407a5cd59d4451c8f
Sam010000101 <reposme@outlook.com>
2025-07-24 11:50:54 +0100

    Testing ammended scripts to enable deployment to github pages and cloudflare pages

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,11 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production";
+const isGitHubPages = process.env.GITHUB_PAGES === "true";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: isProd ? process.env.NEXT_PUBLIC_BASE_PATH : "",
-  assetPrefix: isProd ? process.env.NEXT_PUBLIC_BASE_PATH + "/" : "",
+  basePath: isGitHubPages ? process.env.NEXT_PUBLIC_BASE_PATH : "",
+  assetPrefix: isGitHubPages ? `${process.env.NEXT_PUBLIC_BASE_PATH}/` : "",
   images: {
     remotePatterns: [
       {
